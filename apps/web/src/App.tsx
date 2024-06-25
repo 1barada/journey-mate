3ea9f60c4c0302781263a5e1144d7c2f22a88c5d
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-06-25 20:32:11 +0300

    change route file. Add libraries

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,51 +1,13 @@
-import { lazy } from 'react';
 import { useSelector } from 'react-redux';
-import { createBrowserRouter, Navigate, RouteObject, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Layout } from './components/Layout/Layout';
-import { PrivateRoute } from './components/PrivateRoute';
+import { createRoutes } from './components/Routes';
 import { selectIsAuthenticated } from './store/Auth/AuthSlice';
-
-const HomePage = lazy(() => import('./pages/HomePage/HomePage').then((module) => ({ default: module.HomePage })));
-const EventPage = lazy(() => import('./pages/EventPage/EventPage').then((module) => ({ default: module.EventPage })));
-const ProfilePage = lazy(() =>
-  import('./pages/ProfilePage/ProfilePage').then((module) => ({ default: module.ProfilePage }))
-);
 
 export function App() {
   const isAuthenticated = useSelector(selectIsAuthenticated);
 
-  const routes: RouteObject[] = [
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          path: '/',
-          element: <HomePage />,
-        },
-        {
-          path: '/events',
-          element: <EventPage />,
-        },
-        {
-          path: '/profile',
-          element: <PrivateRoute isAuthenticated={isAuthenticated} />,
-          children: [
-            {
-              path: '',
-              element: <ProfilePage />,
-            },
-          ],
-        },
-        {
-          path: '*',
-          element: <Navigate to="/" replace />,
-        },
-      ],
-    },
-  ];
-
+  const routes = createRoutes(isAuthenticated);
   const router = createBrowserRouter(routes);
 
   return <RouterProvider router={router} />;
