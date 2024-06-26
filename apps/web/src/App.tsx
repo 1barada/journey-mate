074b7f21466f542aa5c3bf3710449bb414f15bda
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-26 11:53:48 +0300

    used npm run format

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,14 +5,10 @@ import { createRoutes } from './components/Routes';
 import { selectIsAuthenticated } from './store/Auth/AuthSlice';
 
 export function App() {
-<<<<<<< HEAD
   const isAuthenticated = useSelector(selectIsAuthenticated);
 
   const routes = createRoutes(isAuthenticated);
   const router = createBrowserRouter(routes);
 
   return <RouterProvider router={router} />;
-=======
-  return <div>Welcome!!!</div>;
->>>>>>> 3869750 (completed ci/cd for web deployment)
 }
