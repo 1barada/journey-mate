8fbcbd93ddd9186a0cd7ef435e144b88898a5c79
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-06-26 11:27:25 +0300

    [WEB] fix issue with default env values

--- apps/web/src/config/env.schema.ts
+++ apps/web/src/config/env.schema.ts
@@ -1,24 +1,14 @@
 import { z } from 'zod';
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
 /**
  * prefix APP_ is must. Can be configured in vite.config.ts file
  * see more https://vitejs.dev/guide/env-and-mode#env-files
  * */
-const APP_SERVER_PORT = z.coerce.number().readonly();
-const APP_SERVER_HOST = z.string().min(1).readonly();
-const APP_PREVIEW_SERVER_PORT = z.coerce.number().readonly();
-const APP_PREVIEW_SERVER_HOST = z.string().min(1).readonly();
-const APP_API_URL = z.string().url().readonly();
-
-if (isDevelopment) {
-  APP_SERVER_PORT.default(4200);
-  APP_PREVIEW_SERVER_PORT.default(4300);
-  APP_SERVER_HOST.default('localhost');
-  APP_PREVIEW_SERVER_HOST.default('localhost');
-  APP_API_URL.default('http://localhost:5000/trcp');
-}
+const APP_SERVER_PORT = z.coerce.number().default(4200);
+const APP_SERVER_HOST = z.string().min(1).default('localhost');
+const APP_PREVIEW_SERVER_PORT = z.coerce.number().default(4300);
+const APP_PREVIEW_SERVER_HOST = z.string().min(1).default('localhost');
+const APP_API_URL = z.string().url().default('http://localhost:5000/trcp');
 
 /**
  * schema with env keys needed in vite.config.ts file
