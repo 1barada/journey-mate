43976797565033272f72a4c9b00393338891f278
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-06-28 01:03:06 +0300

    [WEB] add JourneyMilestone folder + configured to to group related sub-components

---
+++ apps/web/src/components/JourneyMilestone/index.ts
@@ -0,0 +1,6 @@
+export { MilestoneBadge } from './MilestoneBadge';
+export { MilestoneBody } from './MilestoneBody';
+export { MilestoneDatetime } from './MilestoneDatetime';
+export { MilestoneName } from './MilestoneName';
+export { MilestoneSelectButton } from './MilestoneSelectButton';
+export { RouteLine } from './RouteLine';
