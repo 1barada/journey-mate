b79dd9a89e3926213d30e67980a12c3af959b4c3
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-06-28 01:04:26 +0300

    [WEB] add JourneyMilestoneToggle component for selecting milestones user wants to join to

---
+++ apps/web/src/components/JourneyMilestoneToggle/JourneyMilestoneToggleItem.tsx
@@ -0,0 +1,37 @@
+import Stack from '@mui/material/Stack';
+
+import {
+  MilestoneBadge,
+  MilestoneBody,
+  MilestoneDatetime,
+  MilestoneName,
+  MilestoneSelectButton,
+  RouteLine,
+} from '../JourneyMilestone/';
+import type { JourneyMilestoneListItemProps } from '../JourneyMilestone/types';
+
+export const JourneyMilestoneToggleItem: React.FC<JourneyMilestoneListItemProps> = ({
+  milestone,
+  index,
+  itemsCount,
+}) => {
+  const shouldDrawRouteLine = index < itemsCount - 1;
+  const badgeContent = index + 1;
+
+  return (
+    <MilestoneBody key={index}>
+      <MilestoneSelectButton value={milestone.location}>
+        <Stack direction="row" component="span" alignItems="center" gap={1.5}>
+          <MilestoneBadge>{badgeContent}</MilestoneBadge>
+
+          <Stack component="span" paddingRight={1.5}>
+            <MilestoneName>{milestone.location}</MilestoneName>
+            <MilestoneDatetime date={milestone.date} />
+          </Stack>
+        </Stack>
+      </MilestoneSelectButton>
+
+      {shouldDrawRouteLine && <RouteLine />}
+    </MilestoneBody>
+  );
+};
