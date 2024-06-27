9dcfa22c40e748dd2e53da9893f625e573f362d1
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-06-28 01:27:12 +0300

    [WEB] add tooltip and line clamping to MilestoneName component

--- apps/web/src/components/JourneyMilestone/MilestoneName/index.tsx
+++ apps/web/src/components/JourneyMilestone/MilestoneName/index.tsx
@@ -1,18 +1,21 @@
-import type { TypographyProps } from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
-export const MilestoneName: React.FC<TypographyProps> = ({ children, ...props }) => {
+import type { MilestoneNameProps } from './types';
+
+export const MilestoneName: React.FC<MilestoneNameProps> = ({ children, tooltipText, ...props }) => {
   return (
-    <Typography
-      component="span"
-      className="milestone-name"
-      display="block"
-      fontWeight="fontWeightBold"
-      fontSize="20px"
-      textAlign="left"
-      {...props}
-    >
-      {children}
-    </Typography>
+    <Tooltip title={tooltipText}>
+      <Typography
+        component="span"
+        className="milestone-name line-clamp"
+        fontWeight="fontWeightBold"
+        fontSize="20px"
+        textAlign="left"
+        {...props}
+      >
+        {children}
+      </Typography>
+    </Tooltip>
   );
 };
