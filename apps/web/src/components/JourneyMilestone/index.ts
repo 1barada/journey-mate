af50dad10572f6ecc14f0e26c639ac03103604c1
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-06-28 12:57:56 +0300

    [WEB] add MilestoneContentBody and MilestoneDetailsBody to reexport file

--- apps/web/src/components/JourneyMilestone/index.ts
+++ apps/web/src/components/JourneyMilestone/index.ts
@@ -1,6 +1,8 @@
 export { MilestoneBadge } from './MilestoneBadge';
 export { MilestoneBody } from './MilestoneBody';
+export { MilestoneContentBody } from './MilestoneContentBody';
 export { MilestoneDatetime } from './MilestoneDatetime';
+export { MilestoneDetailsBody } from './MilestoneDetailsBody';
 export { MilestoneName } from './MilestoneName';
 export { MilestoneSelectButton } from './MilestoneSelectButton';
 export { RouteLine } from './RouteLine';
