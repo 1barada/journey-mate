49fcf580669b5a57703d88739c6e18f656279bb8
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-28 13:53:34 +0300

    Initialize inner register component structure for modal

---
+++ apps/web/src/components/Register/Register.tsx
@@ -0,0 +1,11 @@
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+// TODO: move interface to shared types.ts file
+interface RegisterProps {
+  temp: string;
+}
+const Register: React.FC<RegisterProps> = ({ temp }) => {
+  return <form></form>;
+};
+
+export { Register };
