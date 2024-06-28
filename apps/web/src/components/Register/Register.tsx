fe8ddc3bbf4da0756121e1dbbc78be11806c721d
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-29 02:14:38 +0300

    Complete basic register form structure and styles

--- apps/web/src/components/Register/Register.tsx
+++ apps/web/src/components/Register/Register.tsx
@@ -1,11 +1,81 @@
-// import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useState } from 'react';
+import { Box, Button, Container, InputLabel, TextField, Typography } from '@mui/material';
 
 // TODO: move interface to shared types.ts file
 interface RegisterProps {
   temp: string;
 }
 const Register: React.FC<RegisterProps> = ({ temp }) => {
-  return <form></form>;
+  const [buttonState, setButtonState] = useState('disabled');
+  return (
+    // TODO: remove box heigh before PR
+    <Container>
+      <Box
+        component="form"
+        height="442px"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography variant="modalHeader" component="h3" color="modal.header">
+          Sign up
+        </Typography>
+        <Box component="div" display="flex" flexDirection="column" width="100%" gap={5}>
+          <Box component="div" display="flex" flexDirection="column" gap={4}>
+            {/* FIXME: create shared input class to remove SX props from input labels */}
+            <InputLabel
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '6px',
+              }}
+            >
+              Email address
+              <TextField fullWidth variant="outlined" />
+            </InputLabel>
+            <InputLabel
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '6px',
+                position: 'relative',
+              }}
+            >
+              Password
+              <TextField fullWidth variant="outlined" />
+            </InputLabel>
+            <InputLabel
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '6px',
+                position: 'relative',
+              }}
+            >
+              Password conformation
+              <TextField fullWidth variant="outlined" />
+            </InputLabel>
+          </Box>
+          <Box component="div" display="flex" flexDirection="column" gap={4}>
+            <Button
+              variant="contained"
+              fullWidth
+              color="secondary"
+              disabled={buttonState === 'disabled' ? true : false}
+              // TODO: discuss with team about shadow styles
+              disableElevation
+              sx={{ py: 3, borderRadius: '30px' }}
+            >
+              <Typography variant="modalBtn" color="modal.text.default">
+                Sing Up
+              </Typography>
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export { Register };
