59188054bc85aac7296908b063cfcd45f3ab6845
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-06-30 20:55:02 +0300

    complete work on register form styles

--- apps/web/src/components/Register/index.tsx
+++ apps/web/src/components/Register/index.tsx
@@ -1,78 +1,47 @@
 import { useState } from 'react';
 import { Box, Button, Container, InputLabel, TextField, Typography } from '@mui/material';
 
+import styles from './styles.module.scss';
 import type { RegisterProps } from './types';
 
 const Register: React.FC<RegisterProps> = ({ temp }) => {
   const [buttonState, setButtonState] = useState('disabled');
   return (
-    // TODO: remove box heigh before PR
-    <Container>
-      <Box
-        component="form"
-        height="442px"
-        display="flex"
-        flexDirection="column"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Typography variant="modalHeader" component="h3" color="modal.header">
-          Sign up
-        </Typography>
-        <Box component="div" display="flex" flexDirection="column" width="100%" gap={5}>
-          <Box component="div" display="flex" flexDirection="column" gap={4}>
-            {/* FIXME: create shared input labels class to remove SX props from input labels */}
-            <InputLabel
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '6px',
-              }}
-            >
-              Email address
-              <TextField fullWidth variant="outlined" />
-            </InputLabel>
-            <InputLabel
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '6px',
-                position: 'relative',
-              }}
-            >
-              Password
-              <TextField fullWidth variant="outlined" />
-            </InputLabel>
-            <InputLabel
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '6px',
-                position: 'relative',
-              }}
-            >
-              Password conformation
-              <TextField fullWidth variant="outlined" />
-            </InputLabel>
-          </Box>
-          <Box component="div" display="flex" flexDirection="column" gap={4}>
-            <Button
-              variant="contained"
-              fullWidth
-              color="secondary"
-              disabled={buttonState === 'disabled' ? true : false}
-              // TODO: discuss with team about shadow styles
-              disableElevation
-              sx={{ py: 3, borderRadius: '30px' }}
-            >
-              <Typography variant="modalBtn" color="modal.text.default">
-                Sing Up
-              </Typography>
-            </Button>
-          </Box>
+    <Box component="form" className={styles.formContainer}>
+      {/* TODO: fix noт-responsive behavior */}
+      <Typography className={styles.formHeader} component="h3" width="480px">
+        Sign up
+      </Typography>
+      <Box className={styles.formContainerFlexColumnGap20} component="div">
+        <Box component="div" className={styles.formContainerFlexColumnGap16}>
+          {/* FIXME: create shared input labels class to remove SX props from input labels */}
+          <InputLabel className={styles.formInputLabel}>
+            Email address
+            <TextField fullWidth variant="outlined" type="email" />
+          </InputLabel>
+          <InputLabel className={styles.formInputLabel}>
+            Password
+            <TextField fullWidth variant="outlined" type="password" />
+          </InputLabel>
+          <InputLabel className={styles.formInputLabel}>
+            Password conformation
+            <TextField fullWidth variant="outlined" type="password" />
+          </InputLabel>
+        </Box>
+        <Box component="div" className={styles.formContainerFlexColumnGap16}>
+          <Button
+            variant="contained"
+            fullWidth
+            className={styles.formSubmitBtn}
+            disabled={buttonState === 'disabled' ? true : false}
+            // TODO: discuss with team about shadow styles
+            disableElevation
+          >
+            <Typography className={styles.formSubmitBtnText}>Sign Up</Typography>
+          </Button>
         </Box>
       </Box>
-    </Container>
+    </Box>
   );
 };
 
