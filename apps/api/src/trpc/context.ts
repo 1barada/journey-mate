dbcb582f399b6558a62dde42b0a654a401ff10ed
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-07-04 19:18:07 +0300

    update context

--- apps/api/src/trpc/context.ts
+++ apps/api/src/trpc/context.ts
@@ -5,17 +5,19 @@ import '@fastify/cookie';
 
 import { prisma } from '../database';
 
+import { Role } from './permissions/permissions';
 import { cookieSchema } from './schemas/cookieSchema';
 import { cookiesValidation } from './utils/cookieValidation';
 
 export function createContext({ req, res }: CreateFastifyContextOptions) {
   const log = req.log;
+  const userTokenData: null | { id: string; role: Role } = null;
 
   const validation = cookiesValidation({ cookieObj: req.cookies, cookiesValidationSchema: cookieSchema });
 
   const validatedCookies = validation.success ? validation.data : null;
 
-  return { req, res, log, db: prisma, validatedCookies };
+  return { req, res, log, db: prisma, validatedCookies, userTokenData };
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>;
