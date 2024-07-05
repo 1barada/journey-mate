93e7378fe2968e44e1fca4d57a2118fa5ff40972
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-07-05 03:23:00 +0300

    add parsed cookies in context.

--- apps/api/src/trpc/context.ts
+++ apps/api/src/trpc/context.ts
@@ -5,8 +5,9 @@ import { prisma } from '../database';
 
 export function createContext({ req, res }: CreateFastifyContextOptions) {
   const log = req.log;
+  const cookies = req.cookies;
 
-  return { req, res, log, db: prisma };
+  return { req, res, log, db: prisma, cookies };
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>;
