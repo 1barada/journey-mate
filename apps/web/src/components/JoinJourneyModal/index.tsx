290bcc2381133a51bde1c5fa5b034e5efe0e5747
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-07-05 13:25:29 +0300

    [WEB] enhance JoinJourneyModal with select all checkbox and separate hook

--- apps/web/src/components/JoinJourneyModal/index.tsx
+++ apps/web/src/components/JoinJourneyModal/index.tsx
@@ -1,4 +1,6 @@
 import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Stack from '@mui/material/Stack';
 
 import './styles.scss';
@@ -8,6 +10,7 @@ import { JourneyMilestoneList } from '../JourneyMilestoneList';
 import { JourneyMilestoneToggle, JourneyMilestoneToggleItem } from '../JourneyMilestoneToggle';
 
 import type { JoinJourneyModalProps } from './types';
+import { useJoinJourneyModal } from './useJoinJourneyModal';
 
 // ! Remove later
 const mockMilestones = [
@@ -21,19 +24,31 @@ const mockMilestones = [
   { location: 'Glazgo', date: '12.05.2025' },
 ];
 
-export const JoinJourneyModal: React.FC<JoinJourneyModalProps> = ({
-  onSelect,
-  initialValue,
-  milestones = mockMilestones,
-  ...props
-}) => {
+export const JoinJourneyModal: React.FC<JoinJourneyModalProps> = ({ milestones = mockMilestones, ...props }) => {
+  const modal = useJoinJourneyModal({ milestones });
+
+  const { onSelectOne, onSelectAll, areAllMilestonesSelected, areSomeMilestonesSelected, selectedMilestones } = modal;
+
   return (
     <Modal title="Select points to join" {...props}>
+      <FormControlLabel
+        label="Select all"
+        className="join-journey-modal__checkbox"
+        control={
+          <Checkbox
+            checked={areAllMilestonesSelected}
+            indeterminate={areSomeMilestonesSelected}
+            onChange={onSelectAll}
+          />
+        }
+      />
+
       <Stack className="join-journey-modal__content">
-        <JourneyMilestoneToggle onSelect={onSelect} initialValue={initialValue}>
+        <JourneyMilestoneToggle onSelect={onSelectOne} value={selectedMilestones}>
           <JourneyMilestoneList milestones={milestones} renderItem={JourneyMilestoneToggleItem} />
         </JourneyMilestoneToggle>
-
+      </Stack>
+      <Stack className="join-journey-modal__footer">
         <Button className="join-journey-modal__confirm-btn">Confirm</Button>
       </Stack>
     </Modal>
