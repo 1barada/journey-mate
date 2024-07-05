d17a5e87102aa609b3747d4847067a35c0740efe
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-07-05 13:23:49 +0300

    [WEB] make JourneyMilestoneToggle component externally controlled

--- apps/web/src/components/JourneyMilestoneToggle/types.ts
+++ apps/web/src/components/JourneyMilestoneToggle/types.ts
@@ -1,6 +1,6 @@
 import type { RouteMilestone } from '../JourneyMilestone/types';
 
 export interface JourneyMilestoneToggleProps extends React.PropsWithChildren {
-  onSelect?: (milestones: Array<RouteMilestone>) => void;
-  initialValue?: Array<RouteMilestone>;
+  onSelect: (milestones: Array<RouteMilestone>) => void;
+  value: Array<RouteMilestone>;
 }
