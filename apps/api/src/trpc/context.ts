8f4f20ca8e418aa7ee538c95e0ec792d0027e0ef
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-07-06 04:25:26 +0300

    update context, add cookies and userToken obj with type definition

--- apps/api/src/trpc/context.ts
+++ apps/api/src/trpc/context.ts
@@ -4,18 +4,18 @@ import { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
 import '@fastify/cookie';
 
 import { prisma } from '../database';
+import { roles } from '../permissions/permissions';
 
-import { Role } from './permissions/permissions';
 import { cookieSchema } from './schemas/cookieSchema';
 import { cookiesValidation } from './utils/cookieValidation';
 
 export function createContext({ req, res }: CreateFastifyContextOptions) {
   const log = req.log;
-  const userTokenData: null | { id: string; role: Role } = null;
 
   const validation = cookiesValidation({ cookieObj: req.cookies, cookiesValidationSchema: cookieSchema });
 
   const validatedCookies = validation.success ? validation.data : null;
+  const userTokenData: null | { userId: string; userRole: roles } = null;
 
   return { req, res, log, db: prisma, validatedCookies, userTokenData };
 }
