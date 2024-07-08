695e952066c965163869eff8733e10a1e7e79c3a
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-07-08 12:20:40 +0300

    add about page info functionality

---
+++ apps/web/src/components/AboutPageInfo/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Box, Container, Typography } from '@mui/material';
+
+import styles from './AboutPageInfo.module.scss';
+import type { AboutPageInfoProps } from './types';
+
+export const AboutPageInfo: FC<AboutPageInfoProps> = ({ info }) => {
+  return (
+    <Box component="section" className={styles.section}>
+      <Container>
+        <Box component="div" className={styles.infoWrapper}>
+          <Typography component="h1" className={styles.title}>
+            {info}
+          </Typography>
+          <NavLink to="/" className={styles.link}>
+            Back
+          </NavLink>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
--- apps/web/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/web/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,10 @@
+import { AboutPageInfo } from '../../components/AboutPageInfo';
 import { Profile } from '../../components/Profile';
 
 const ProfilePage = () => {
   return (
     <div>
+      <AboutPageInfo info="Your profile" />
       <Profile />
     </div>
   );
