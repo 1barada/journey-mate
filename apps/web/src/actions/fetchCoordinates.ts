9ca3b90063d61aa402fd07e6cfb9aa130efbcf2a
Andrii Kuluiev <andreykuluev@gmail.com>
2024-07-15 16:24:18 +0300

    Delete throwing an error

--- apps/web/src/actions/fetchCoordinates.ts
+++ apps/web/src/actions/fetchCoordinates.ts
@@ -12,17 +12,16 @@ export const fetchCoordinates = async (inputValue: string) => {
     });
 
     const results = response.data.results;
-    if (results && results.length > 0) {
-      const location = results[0].geometry.location;
-      const coordinates = {
-        lat: location.lat,
-        lng: location.lng,
-      };
 
-      return coordinates;
-    }
+    if (!results || results.length === 0) return null;
 
-    throw new Error('No results found');
+    const location = results[0].geometry.location;
+    const coordinates = {
+      lat: location.lat,
+      lng: location.lng,
+    };
+
+    return coordinates;
   } catch (error) {
     console.error('Error fetching coordinates:', error);
   }
