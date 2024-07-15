3a1aa4a57f685fa78262ad077f89a4bbb3530796
Andrii Kuluiev <andreykuluev@gmail.com>
2024-07-15 15:48:46 +0300

    Fix fetchCoordinates action

--- apps/web/src/components/SearchLocationInput/index.tsx
+++ apps/web/src/components/SearchLocationInput/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
+import { debounce } from 'lodash';
 
 import { fetchCoordinates } from '../../actions/fetchCoordinates';
 
@@ -13,17 +14,25 @@ export const SearchLocationInput: React.FC<SearchLocationInputProps> = ({ onPlac
   useEffect(() => {
     if (inputRef.current) {
       const input = (inputRef.current as HTMLElement).querySelector('input');
-      if (input) {
-        const handleInputChange = () => {
-          fetchCoordinates(input.value, onPlaceSelected);
-        };
 
-        input.addEventListener('input', handleInputChange);
-
-        return () => {
-          input.removeEventListener('input', handleInputChange);
-        };
+      if (!input) {
+        return;
       }
+
+      const handleInputChange = debounce(async () => {
+        if (!input.value) return;
+        const coordinates = await fetchCoordinates(input.value);
+        if (!coordinates) {
+          return;
+        }
+        onPlaceSelected(coordinates, input.value);
+      }, 2000);
+
+      input.addEventListener('input', handleInputChange);
+
+      return () => {
+        input.removeEventListener('input', handleInputChange);
+      };
     }
   }, []);
 
