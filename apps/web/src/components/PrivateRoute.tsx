1ea709750c0c234889425dc535aaf8668c4dab32
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-07-16 11:50:22 +0300

    [WEB] rework private route component

--- apps/web/src/components/PrivateRoute.tsx
+++ apps/web/src/components/PrivateRoute.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
+import { selectIsAuthenticated } from '../store/Auth/AuthSlice';
+import { useAppSelector } from '../types/reduxTypes';
+
 interface PrivateRouteProps {
-  isAuthenticated: boolean;
+  className?: string;
 }
 
-export const PrivateRoute: React.FC<PrivateRouteProps> = ({ isAuthenticated }) => {
+export const PrivateRoute: React.FC<PrivateRouteProps> = () => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
+
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
