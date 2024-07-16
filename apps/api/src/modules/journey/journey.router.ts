3826933cf20a3e621567d5bf57897e4034634d73
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-07-16 22:44:09 +0300

    [API] replace public procedure with authorizedProcedure

--- apps/api/src/modules/journey/journey.router.ts
+++ apps/api/src/modules/journey/journey.router.ts
@@ -1,10 +1,14 @@
-import { publicProcedure, router } from '../../trpc/trpc';
+import { authorizedProcedure, router } from '../../trpc/trpc';
+import { PermissionAction, PermissionEntity } from '../auth/domain/enums/permissions.enums';
 
 import { CreateJourneyWithUserIdSchema, JourneySchema } from './domain/entities/journey.entity';
 import { createJourneyService } from './service/journey/journey.factory';
 
 export const journeyRouter = router({
-  createJourney: publicProcedure
+  createJourney: authorizedProcedure({
+    requiredAction: PermissionAction.Create,
+    requiredEntity: PermissionEntity.Event,
+  })
     .input(CreateJourneyWithUserIdSchema)
     .output(JourneySchema)
     .mutation(async ({ input, ctx }) => {
