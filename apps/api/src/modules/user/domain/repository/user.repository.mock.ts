fd2c3e3b60da631ece058b667053d2e291f19ddb
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-07-20 14:22:03 +0300

    uncomment

--- apps/api/src/modules/user/domain/repository/user.repository.mock.ts
+++ apps/api/src/modules/user/domain/repository/user.repository.mock.ts
@@ -2,17 +2,17 @@ import { vi } from 'vitest';
 
 import { FindUserByEmailResult, UserRepositoryPort } from './user.repository';
 
-// export const defaultUserRepositoryMock: UserRepositoryPort = {
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   findUserByEmail: vi.fn<any, Promise<FindUserByEmailResult>>().mockResolvedValue({
-//     id: 1,
-//     active: true,
-//     authProvider: 'password',
-//     email: 'user@mail.com',
-//     name: 'John Doe',
-//     passwordHash: '$2b$10$pKokDDJDr40sHA0aZZbI6OnTzqwRHGgAZvTU8Q1HnP5k1enpCtBzS', // password: 12345678
-//     role: 'admin',
-//   }),
-//   confirmUserAccount: vi.fn().mockResolvedValue({}),
-//   createUserWithEmail: vi.fn().mockResolvedValue({}),
-// };
+export const defaultUserRepositoryMock: UserRepositoryPort = {
+  //   eslint-disable-next-line @typescript-eslint/no-explicit-any
+  findUserByEmail: vi.fn<any, Promise<FindUserByEmailResult>>().mockResolvedValue({
+    id: 1,
+    active: true,
+    authProvider: 'password',
+    email: 'user@mail.com',
+    name: 'John Doe',
+    passwordHash: '$2b$10$pKokDDJDr40sHA0aZZbI6OnTzqwRHGgAZvTU8Q1HnP5k1enpCtBzS', // password: 12345678
+    role: 'admin',
+  }),
+  confirmUserAccount: vi.fn().mockResolvedValue({}),
+  createUserWithEmail: vi.fn().mockResolvedValue({}),
+};
