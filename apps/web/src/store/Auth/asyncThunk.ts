de51554a5747d0313631c22d2bae5b32977dbc26
AsphaltHedgehog <svopyp@gmail.com>
2024-07-20 18:35:16 +0300

    Complete

---
+++ apps/web/src/store/Auth/asyncThunk.ts
@@ -0,0 +1,63 @@
+import { toast } from 'react-toastify';
+import type { ReducerCreators } from '@reduxjs/toolkit';
+
+import { FormInputsTypes } from '../../components/Forms/Login/types';
+import { trpcClient } from '../../services/trpc';
+
+import type { IAuthSlice } from './types';
+
+export const loginAsyncThunk = (creator: ReducerCreators<IAuthSlice>) =>
+  creator.asyncThunk(
+    async (data: FormInputsTypes, { rejectWithValue }) => {
+      try {
+        await trpcClient.user.login.mutate(data);
+        return;
+      } catch (error) {
+        return rejectWithValue((error as Error).message);
+      }
+    },
+    {
+      pending: (state) => {
+        state.isLoading = true;
+        state.error = null;
+      },
+      fulfilled: (state) => {
+        state.isLoading = false;
+        state.error = null;
+        toast.success('Welcome back!');
+      },
+      rejected: (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload as string;
+        toast.error(payload as string);
+      },
+    }
+  );
+
+export const registerAsyncThunk = (creator: ReducerCreators<IAuthSlice>) =>
+  creator.asyncThunk(
+    async (data: FormInputsTypes, { rejectWithValue }) => {
+      try {
+        await trpcClient.user.registerWithEmail.mutate({ ...data });
+        return;
+      } catch (error) {
+        return rejectWithValue((error as Error).message);
+      }
+    },
+    {
+      pending: (state) => {
+        state.isLoading = true;
+        state.error = null;
+      },
+      fulfilled: (state) => {
+        state.isLoading = false;
+        state.error = null;
+        toast.success("You've successfully registered! Please proceed to your email to activate your account.");
+      },
+      rejected: (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload as string;
+        toast.error(payload as string);
+      },
+    }
+  );
