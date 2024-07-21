27fec01d690b94ffa395be7c17ee29bcfab2e9ee
AsphaltHedgehog <svopyp@gmail.com>
2024-07-21 23:01:03 +0300

    fix zodSchema

--- apps/api/src/modules/notification/domain/entities/notificationEvent.entity.ts
+++ apps/api/src/modules/notification/domain/entities/notificationEvent.entity.ts
@@ -5,7 +5,7 @@ export const NotificationEventTypeSchema = z.enum(['joinRequest', 'chatMessage',
 export const NotificationEventSchema = z.object({
   id: z.number(),
   notificationId: z.number(),
-  userId: z.number().optional(),
+  userId: z.number().nullable(),
   type: NotificationEventTypeSchema,
   createdAt: z.date(),
 });
