28033f008e3c916ca57ffd8c9863b16f72249395
AsphaltHedgehog <svopyp@gmail.com>
2024-07-21 22:48:34 +0300

    separate entities

--- apps/api/src/modules/notification/domain/entities/notification.entity.ts
+++ apps/api/src/modules/notification/domain/entities/notification.entity.ts
@@ -1,16 +1,6 @@
 import { z } from 'zod';
 
-export const NotificationEventTypeSchema = z.enum(['joinRequest', 'chatMessage', 'chatMessageMention']);
-
-export const NotificationEventSchema = z.object({
-  id: z.number(),
-  notificationId: z.number(),
-  userId: z.number().optional(),
-  type: NotificationEventTypeSchema,
-  createdAt: z.date(),
-});
-
-export type NotificationEvent = z.infer<typeof NotificationEventSchema>;
+import { NotificationEventSchema } from './notificationEvent.entity';
 
 export const NotificationSchema = z.object({
   id: z.number(),
