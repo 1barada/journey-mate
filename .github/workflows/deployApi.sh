782a50d7db9466334eebc667f0ba8143ece250ec
AsphaltHedgehog <svopyp@gmail.com>
2024-07-23 02:58:17 +0300

    fix ssh

--- .github/workflows/deployApi.sh
+++ .github/workflows/deployApi.sh
@@ -1,6 +1,5 @@
 set -e
 
-
 host=$1
 username=$2
 key=$3
@@ -8,7 +7,7 @@ AWS_ECR_REGISTRY=$4
 AWS_ECR_REPOSITORY=$5
 AWS_IMAGE_TAG=$6
 
-ssh -i "$AWS_EC2_SSH_KEY" "$AWS_EC2_USER@$AWS_EC2_HOST" << EOF
+ssh -i "$key" "$username@$host" << EOF
   docker pull $AWS_ECR_REGISTRY/$AWS_ECR_REPOSITORY:$AWS_IMAGE_TAG
   docker stop journey || true
   docker rm journey || true
