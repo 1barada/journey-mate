d94291992d2bb37305b987f45c007c5b36ff9a6c
AsphaltHedgehog <svopyp@gmail.com>
2024-07-22 16:09:37 +0300

    adjusted service to return proper objects in response

--- apps/api/src/modules/notification/domain/service/notification.service.ts
+++ apps/api/src/modules/notification/domain/service/notification.service.ts
@@ -6,9 +6,11 @@ import type {
   DeleteNotificationEventParams,
   DeleteNotificationEventResult,
   GetAllNotificationParams,
-  GetAllNotificationResult,
+  GetAllNotificationsResult,
   GetNotificationEventsParams,
   GetNotificationEventsResult,
+  GetNotificationParams,
+  GetNotificationResult,
   NotificationUsecase,
 } from '../../domain/usecases/notification.usecase';
 import { NotificationRepositoryPort } from '../repository/notification.repository';
@@ -16,28 +18,86 @@ import { NotificationRepositoryPort } from '../repository/notification.repositor
 export class NotificationService implements NotificationUsecase {
   constructor(private db: NotificationRepositoryPort) {}
 
-  async getAllNotifications(params: GetAllNotificationParams): Promise<GetAllNotificationResult> {
-    return (await this.db.getAllNotifications({ id: params.userId })) as GetAllNotificationResult;
+  async getNotification(params: GetNotificationParams): Promise<GetNotificationResult> {
+    const response = await this.db.getNotification({ id: params.id });
+    if (!response) {
+      return null;
+    }
+
+    const { _count, events, id, journey, journeyId, userId } = response;
+    const formattedNotification = {
+      id,
+      journeyId,
+      title: journey.title,
+      totalEvents: _count.events,
+      userId,
+      events,
+    };
+
+    return formattedNotification;
+  }
+
+  async getAllNotifications(params: GetAllNotificationParams): Promise<GetAllNotificationsResult> {
+    const response = await this.db.getAllNotifications({ id: params.userId });
+
+    if (!response) {
+      return [];
+    }
+
+    const nonEmptyNotifications = response.filter((n) => n._count.events > 0);
+
+    if (nonEmptyNotifications.length === 0) {
+      return [];
+    }
+
+    const formattedNotifications = nonEmptyNotifications.map((n) => {
+      return {
+        id: n.id,
+        journeyId: n.journeyId,
+        title: n.journey.title,
+        totalEvents: n._count.events,
+        userId: n.userId,
+      };
+    });
+
+    return formattedNotifications;
   }
   async getNotificationEvents(params: GetNotificationEventsParams): Promise<GetNotificationEventsResult> {
-    return (await this.db.getNotificationEvents({
+    const response = await this.db.getNotificationEvents({
       notificationId: params.notificationId,
-    })) as GetNotificationEventsResult;
+    });
+
+    if (!response) {
+      return [];
+    }
+
+    const formattedEvents = response.map((n) => {
+      return {
+        id: n.id,
+        notificationId: n.notificationId,
+        type: n.type,
+        user: n.user?.name ?? null,
+        userId: n.userId ?? null,
+        createdAt: n.createdAt,
+      };
+    });
+
+    return formattedEvents;
   }
   async deleteNotificationEvent(params: DeleteNotificationEventParams): Promise<DeleteNotificationEventResult> {
-    return (await this.db.deleteNotificationEvent({ id: params.id })) as DeleteNotificationEventResult;
+    return await this.db.deleteNotificationEvent({ id: params.id });
   }
   async createNotification(params: CreateNotificationParams): Promise<CreateNotificationResult> {
-    return (await this.db.createNotification({
+    return await this.db.createNotification({
       journeyId: params.journeyId,
       userId: params.userId,
-    })) as CreateNotificationResult;
+    });
   }
   async createNotificationEvent(params: CreateNotificationEventParams): Promise<CreateNotificationEventResult> {
-    return (await this.db.createNotificationEvent({
+    return await this.db.createNotificationEvent({
       userId: params.userId,
       notificationId: params.userId,
       type: params.type,
-    })) as CreateNotificationEventResult;
+    });
   }
 }
--- apps/api/src/modules/notification/domain/usecases/notification.usecase.ts
+++ apps/api/src/modules/notification/domain/usecases/notification.usecase.ts
@@ -1,30 +1,60 @@
 import { Notification } from '../entities/notification.entity';
 import { NotificationEvent, NotificationEventTypeSchema } from '../entities/notificationEvent.entity';
 
+export type GetNotificationParams = {
+  id: string;
+};
+
+export type GetNotificationResult = {
+  id: number;
+  journeyId: number;
+  title: string;
+  totalEvents: number;
+  userId: number;
+  events: NotificationEvent[] | [];
+} | null;
+
 export type GetAllNotificationParams = {
   userId: string;
 };
 
-export type GetAllNotificationResult = Notification[] | null;
+export type GetAllNotificationsResult =
+  | {
+      id: number;
+      journeyId: number;
+      title: string;
+      totalEvents: number;
+      userId: number;
+    }[]
+  | [];
 
 export type GetNotificationEventsParams = {
   notificationId: string;
 };
 
-export type GetNotificationEventsResult = NotificationEvent[] | [];
+export type GetNotificationEventsResult =
+  | {
+      id: number;
+      notificationId: number;
+      type: string;
+      userId: number | null;
+      user: string | null;
+      createdAt: Date;
+    }[]
+  | [];
 
 export type DeleteNotificationEventParams = {
   id: string;
 };
 
-export type DeleteNotificationEventResult = NotificationEvent;
+export type DeleteNotificationEventResult = NotificationEvent | null;
 
 export type CreateNotificationParams = {
   journeyId: string;
   userId: string;
 };
 
-export type CreateNotificationResult = Notification;
+export type CreateNotificationResult = Notification | null;
 
 export type CreateNotificationEventParams = {
   journeyId: string;
@@ -32,10 +62,10 @@ export type CreateNotificationEventParams = {
   type: keyof typeof NotificationEventTypeSchema.Values;
 };
 
-export type CreateNotificationEventResult = NotificationEvent;
+export type CreateNotificationEventResult = NotificationEvent | null;
 
 export interface NotificationUsecase {
-  getAllNotifications(params: GetAllNotificationParams): Promise<GetAllNotificationResult>;
+  getAllNotifications(params: GetAllNotificationParams): Promise<GetAllNotificationsResult>;
   getNotificationEvents(params: GetNotificationEventsParams): Promise<GetNotificationEventsResult>;
   deleteNotificationEvent(params: DeleteNotificationEventParams): Promise<DeleteNotificationEventResult>;
   createNotification(params: CreateNotificationParams): Promise<CreateNotificationResult>;
