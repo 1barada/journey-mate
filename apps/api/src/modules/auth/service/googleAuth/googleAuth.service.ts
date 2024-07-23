489c78932c73a1aa6f77698cd87f7b32fac418d0
Sergey Kabanets <skabanets97@gmail.com>
2024-07-23 21:24:31 +0300

    Add googleAuth service

---
+++ apps/api/src/modules/auth/service/googleAuth/googleAuth.service.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+import { config } from '@project/api/config';
+import { verifyGoogleToken } from '@project/api/trpc/utils/verifyGoogleToken';
+import jwt from 'jsonwebtoken';
+
+export class GoogleAuthService {
+  constructor(private prisma: PrismaClient) {}
+
+  async googleAuth(token: string) {
+    const { email, name } = await verifyGoogleToken(token);
+
+    if (!email) {
+      throw new Error('Google token verification failed: email is missing');
+    }
+
+    let user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          email,
+          name: name || '',
+          authProvider: 'socials',
+          active: true,
+        },
+      });
+    } else if (user.authProvider !== 'socials') {
+      throw new Error('User registered with another method');
+    }
+
+    const userResponse = {
+      email: user.email,
+      name: user.name || '',
+    };
+
+    const accessToken = this.createAccessToken(user.id, user.email);
+    return { user: userResponse, token: accessToken };
+  }
+
+  private createAccessToken(userId: number, email: string): string {
+    const token = jwt.sign({ userId: userId.toString(), email }, config.get('cookieSecret') as string, {
+      expiresIn: '6h',
+    });
+    return token;
+  }
+}
