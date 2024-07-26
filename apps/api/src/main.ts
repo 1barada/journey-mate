f7c773f9a0fd754146516f79f73f3f92eac5a1cd
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-07-26 18:34:55 +0300

    change fetch to axios

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,19 +13,19 @@ import { server } from './server';
 const host = config.get('host');
 const port = config.get('port');
 
-// const origin = config.get('frontendUrl');
+const origin = config.get('frontendUrl');
 
-// server.register(cors, {
-//   origin: [origin],
-//   credentials: true,
-// });
+server.register(cors, {
+  origin: [origin],
+  credentials: true,
+});
 
-if (config.get('nodeEnv') == 'development') {
-  server.register(cors, {
-    origin: ['*'],
-    credentials: true,
-  });
-}
+// if (config.get('nodeEnv') == 'development') {
+//   server.register(cors, {
+//     origin: ['*'],
+//     credentials: true,
+//   });
+// }
 
 server.register(fastifyCookie, {
   secret: config.get('cookieSecret'),
